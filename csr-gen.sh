38481549673c60bb52ec6500ce0d6ee23eed78dc
Wei Wang <ww@9rivers.com>
2020-03-31 18:38:12 -0400

    Remove other scripts from csr-gen project

--- csr-gen.sh
+++ csr-gen.sh
@@ -1,19 +1,74 @@
 #!/bin/sh
 
+##	Defaults:	----------------------------------------------------------
+KEYSIZE=${KEYSIZE:-4096}
+DIGEST=${DIGEST:-"sha256"}
+C=${C:-US}
+ST=${ST:-Michigan}
+L=${L:-Ann Arbor}
+O=${O:-Michigan Medicine}
+OU=${OU:-HITS}
+GROUPEMAIL=${GROUPEMAIL:-"HITS-Performance@umich.edu"}
+CONFVER=version002
+
+##	Variables & parameters:
+HOSTNAME=''
+CSRCONF=''
+opt_old_conf=''
+opt_old_key=''
+args=''
+
+##--------------------------------------------------------------------------------
+##	Display a help message for the script
+##--------------------------------------------------------------------------------
 usage() {
 	cat <<EOF
 
-TLS Key & CSR Generator
+TLS Key & Certificate Signing Request Generator
 VERSION	2.0
 
-	Usage:	$0 "<hostname>" ["<alternative-names>"]
+	Usage:	$0 [<OPTIONS>] "<hostname>" ["<alternative-names>"]
+
+	The given hostname (first parameter on the command line) is taken as the name
+	of the subject to be certified. Key and CSR files are named with the hostname
+	with .key and .csr extensions.
 
 	Alternative-names may be provided optionally (Defaults to none), in FQDN format:
 
 		<alt-name-1> <alt-name-2> <alt-name-3> ...
 
 	Where, <alt-name-1>, <alt-name-2>, <alt-name-3>, etc. are aliases (CNAMEs) of
-	the <hostname>.
+	the <hostname> -- This allows the certificate-key pair to be used on all these
+	hosts.
+
+	Optionally, a CSR config file is generated for the CSR, which may be used in the
+	future to add more host names to the certificate. If no alternative-name is given
+	and a config file exists, it will be left unchanged and used to create the CSR.
+
+OPTIONS
+
+	-h --help		Print this help message.
+	-k --old-key		Use existing old key, rather than create new (default).
+	-o --old-config		Use existing old config, rather than create new (default).
+
+ENVIRONMENT VARIABLES
+
+	C		Country name
+	ST		State name
+	L		Name of your locale (city)
+	O		Name of your organization
+	OU		Nmae of your unit within the organizational
+	GROUPEMAIL	The email address to receive notification regarding the cert
+	KEYSIZE		Size of key to generate
+	DIGEST		Message Digest Method
+
+FILES
+
+	Configuration files in ~/.etc or ~/.config folder are used:
+
+	csr-gen.rc		Environment variable settings for this script.
+
+	csr/<hostname>.conf	Per-host CSR configuration.
 
 DEPENDENCY
 
@@ -21,25 +76,48 @@ DEPENDENCY
 
 EOF
 	exit 2
-}
+} # usage
 
-HOSTNAME=$1
-if [ "$HOSTNAME" = "" ]; then
+no_hostname() {
 	echo "Expecting hostname in FQDN." >&2
 	usage
-fi
-if [ "$1" = "-h" -o "$1" = '--help' ]; then
-	usage
-fi
+}
+
+##--------------------------------------------------------------------------------
+##	Process command line options:
+##--------------------------------------------------------------------------------
+get_opts() {
+    local xvar
+    for xvar in $@; do
+	case "$xvar" in
+	    ("-o"|"--old-config")	opt_old_conf='x';;
+	    ("-k"|"--old-key")		opt_old_key='x';;
+	    ("-h"|'--help')		usage;;
+	    (*)
+		if [ "$HOSTNAME" = "" ]; then
+		    HOSTNAME="$xvar"
+		    CSRCONF="${xvar}.conf"
+		elif [ "$args" = "" ]; then
+		    args="$xvar"
+		else
+		    args="${args} ${xvar}"
+		fi;;
+	esac
+    done
+} # get_opts
+get_opts $@
+if [ "$HOSTNAME" = "" ]; then no_hostname; fi
 
 #	Allow this script to be configured, either in ~/.config/ or ~/.etc/:
 CONFDIR="$HOME/.etc"
 if [ ! -d "$CONFDIR" ]; then CONFDIR="$HOME/.config"; fi
 CONF="$CONFDIR/csr-gen.rc"
+csr_dir="${CONFDIR}/csr"
+csr_cf="${csr_dir}/${CSRCONF}"
 if [ -r "$CONF" ]; then . "$CONF"; fi
-if [ "$OU" = "" -o "$GROUPEMAIL" = "" ]; then
+if [ "$CSR_GEN_RC" != "$CONFVER" -o "$GROUPEMAIL" = "" ]; then
     mkdir -p "$CONFDIR"
-    echo 'CSR_GEN_RC=configured
+    echo "CSR_GEN_RC=${CONFVER}"'
 #
 #	Organizational Unit, with these components:
 #
@@ -48,59 +126,94 @@ if [ "$OU" = "" -o "$GROUPEMAIL" = "" ]; then
 #		L	Name of your locale (city)
 #		O	Name of your organization
 #		OU	Nmae of your unit within the organizational
-if [ "$OU" = "" ]; then
-    OU="/C=US/ST=Michigan/L=Ann Arbor/O=University of Michigan/OU=UMHS-HITS-EI"
-fi
+C=${C:-US}
+ST=${ST:-Michigan}
+L=${L:-Ann Arbor}
+O=${O:-University of Michigan}
+OU=${OU:-HITS}
 
 #
 #	Group email as contact for certificates.
 #
 #	For InCommon certificates, ITS requires that the group exists in the
 #	UMICH.EDU directory.
-if [ "$GROUPEMAIL" = "" ]; then
-    GROUPEMAIL="emailAddress=HITS-Performance@umich.edu"
-fi
+GROUPEMAIL="${GROUPEMAIL:-HITS-Performance@umich.edu}"
 
 #
 #	Options: Default values are shown below.
 #
-#KEYSIZE=4096
-#SHA256="-sha256"
-' >>"$CONF" && cat <<EOF
+#KEYSIZE=${KEYSIZE:-4096}
+#DIGEST="${DIGEST:-sha256}"
 
-A configuration file "$CONF" has been created for you.
+#
+#	Extra success / failure Messages
+#
+SUCCESS_MESSAGE="
+------	CSR created successfully. Submit the request here for InCommon certification:
+
+	https://cert-manager.com/customer/InCommon/ssl
+"' >"$CONF" && cat <<EOF
+
+A configuration file "$CONF" has been created / updated for you.
 
 Please chack and make sure that the contents are correct for your needs.
+
 EOF
     exit 3
 fi
 
-# subjectAltName is changed to take <alt-name-1> <alt-name-2> <alt-name-3> . . .
-# directly on the command line:
-ADDEXT=""
-SUBJECTALTNAME=""
-shift
-for ix in "$@"; do
-	if [ "$SUBJECTALTNAME" != "" ]; then SUBJECTALTNAME="$SUBJECTALTNAME, "; fi
-	SUBJECTALTNAME="${SUBJECTALTNAME}DNS:${ix}"
-done
-if [ "$SUBJECTALTNAME" != "" ]; then
-    ADDEXT="-addext"
-    SUBJECTALTNAME="subjectAltName = '$SUBJECTALTNAME'"
-fi
+##--------------------------------------------------------------------------------
+##	Generate a CSR configuration file.
+##--------------------------------------------------------------------------------
+config_csr() {
+    if [ "$opt_old_config" = 'x' -o "$args" = '' ]; then
+	if [ -f "$csr_cf" ]; then
+	    echo Using existing/old CSR configuration.\n
+	    return
+	fi
+    fi
+    mkdir -p "${csr_dir}"
+    cat >"${csr_cf}" <<EOF
+#	Config file for ${HOSTNAME}.csr
+[req]
+default_bits		= ${KEYSIZE}
+default_md		= ${DIGEST}
+req_extensions		= req_ext
+distinguished_name	= dn
+prompt			= no
+encrypt_key		= no
 
-KEYSIZE=${KEYSIZE:-4096}
-if [ "$SHA256"="" ]; then SHA256="-sha256"; fi
+[dn]
+C="$C"
+ST="$ST"
+L="$L"
+O="$O"
+OU="$OU"
+emailAddress="$GROUPEMAIL"
+CN="${HOSTNAME}"
+
+[req_ext]
+subjectAltName = @alt_names
+
+[alt_names]
+EOF
+    shift
+    local ix=0
+    for xvar in $args; do
+	echo DNS.${ix} = ${xvar} >>"${csr_cf}"
+	ix=`expr ${ix} + 1`
+    done
+} # config_csr
+
 KEYFILE="${HOSTNAME}.key"
 CSRFILE="${HOSTNAME}.csr"
 
-COMMONNAME="$OU/CN=$HOSTNAME"
+COMMONNAME="$ORGANIZATIONALUNIT/CN=$HOSTNAME"
 cat <<EOF
 
 Generating TLS key and certification request for:
 
   HOSTNAME = ${HOSTNAME}
-  AltNames =${SUBJECTALTNAME}
   Key size = ${KEYSIZE}
 
      Email = ${GROUPEMAIL}
@@ -108,19 +221,29 @@ COMMONNAME = ${COMMONNAME}
 
   Key file = ${KEYFILE}
   CSR file = ${CSRFILE}
+
+Find more details in "${csr_cf}".
 EOF
 
-openssl genrsa -out "$KEYFILE" "$KEYSIZE"
-if openssl req -new "$SHA256" -nodes -key "$KEYFILE" -out "$CSRFILE" \
-	   -subj "${COMMONNAME}/${GROUPEMAIL}" $ADDEXT "${SUBJECTALTNAME}"
+config_csr $@
+if [ "$HOSTNAME" = '' ]; then no_hostname; fi
+
+echo opt_old_key = $opt_old_key
+if [ -f "$KEYFILE" -a "$opt_old_key" = 'x' ]; then
+    echo "Using existing key file: [$KEYFILE]"
+else
+    openssl genrsa -out "$KEYFILE" "$KEYSIZE"
+fi
+if openssl req -new -nodes -key "$KEYFILE" -out "$CSRFILE" -config "$csr_cf"
 then
 	cat <<EOM
 
-------	CSR created successfully. Submit the request here for InCommon certification:
+------	Run command below to see the contents of the CSR:
+	openssl req -text -noout -in "${CSRFILE}"
 
-	https://webservices.itcs.umich.edu/index.php?screen=request&service=ssl_certificate
-
+${SUCCESS_MESSAGE}
 EOM
 	exit 0
 fi
+echo ${FAILURE_MESSAGE}
 exit 1
